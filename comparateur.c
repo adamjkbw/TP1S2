9bf1a5d5a9c2a36234daffbe2b54f9119ffa78bd
Adam Jakubowski <adam.jakubowski@student.junia.com>
2026-02-23 13:36:49 +0100

    Lecture

--- comparateur.c
+++ comparateur.c
@@ -10,3 +10,16 @@ void saisieDeDonnees(int nb1, int nb2, char fich[])
     printf("Entrez le chemin du fichier texte (il doit être au même niveau que l'exe): ");
     scanf("%s", &fich);
 }
+
+void lectureListe(char chemin[25])
+{
+    char line[256];
+    char nom[128], prenom[128];
+    while (fgets(line, sizeof(line), f))
+    {
+        // Attejd "prenom nom" (séparés par espaces ou tab)
+        if (sscanf(line, " %127s %127s", prenom, nom) == 2)
+        {
+        }
+    }
+}
